d1ed95f58433f7f6f78da4f28c4dd946bf7ff8bf
Gorlenochka <koshkalena88@gmail.com>
2022-10-21 22:26:09 +0300

    Added search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState} from "react";
 
 export default function Dictionary() {
+    let [searchingWorld, setSearchingWorld] = useState("");
+    function search(event) {
+        event.preventDefault();
+        alert(`${ searchingWorld}`);
+    }
+    function showWorld(event) {
+        setSearchingWorld(event.target.value);
+    }
     return (
         <div className="Dictionary">
             <h1 className="DictionaryTitle">Dictionary</h1>
-            <form className="SearchForm">
-                <input type="search" placeholder="Search for any word or phrase"></input>
+            <form className="SearchForm" onSubmit={search}>
+                <input type="search" placeholder="Search for any word or phrase" onChange={showWorld}></input>
                 <button className="SearchButton">Search</button>
             </form>
         </div>
